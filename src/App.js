f8c0560eb8874681c1162817a7b81357e0fe1855
Your name here <mtdamyanova@gmail.com>
2021-03-19 11:38:25 +0200

    creating ROUTER

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
 import Slidebar from './components/Slidebar/Slidebar';
@@ -6,16 +12,22 @@ import WhatshotIcon from '@material-ui/icons/Whatshot';
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import HistoryIcon from '@material-ui/icons/History';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Slidebar Icon={HomeIcon} type={'Home'} />
-      <Slidebar Icon={WhatshotIcon} type={'Trending'} />
-      <Slidebar Icon={VideoLibraryIcon} type={'Library'} />
-      <Slidebar Icon={HistoryIcon} type={'History'} />
-    </div>
+    <Router>
+      <>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Slidebar Icon={HomeIcon} type={'Home'} />
+            <Slidebar Icon={WhatshotIcon} type={'Trending'} />
+            <Slidebar Icon={VideoLibraryIcon} type={'Library'} />
+            <Slidebar Icon={HistoryIcon} type={'History'} />
+          </Route>
+        </Switch>
+      </>
+    </Router>
   );
 }
 
-export default App;
